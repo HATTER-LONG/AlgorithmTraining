bfc204079223bcf1cb24936f292af24ce3d48d7d
HATTER-LONG <caolei6767@gmail.com>
2021-03-02 22:20:02 +0800

    98题提交

---
+++ Src/98_ValidateBinarySearchTree/ValidateBinarySearchTree.cpp
@@ -0,0 +1,89 @@
+#include "Tools/BinaryTreeNodeTools.h"
+
+#include <catch2/catch.hpp>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <limits.h>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+using namespace Catch;
+using namespace std;
+
+//中序遍历
+// 中序遍历必然有序，如无序则不符合
+class Solution
+{
+public:
+    bool isValidBST(TreeNode* root)
+    {
+        stack<TreeNode*> stack;
+        long long inorder = LONG_MIN;
+
+        while (!stack.empty() || root != nullptr)
+        {
+            while (root != nullptr)
+            {
+                stack.push(root);
+                root = root->left;
+            }
+            root = stack.top();
+            stack.pop();
+            // 如果中序遍历得到的节点的值小于等于前一个 inorder，说明不是二叉搜索树
+            if (root->val <= inorder)
+            {
+                return false;
+            }
+            inorder = root->val;
+            root = root->right;
+        }
+        return true;
+    }
+};
+
+//递归
+// 注意其上界 下界如何保存
+class Solution_Depth
+{
+public:
+    bool helper(TreeNode* root, long long lower, long long upper)
+    {
+        if (root == nullptr)
+            return true;
+
+        if (root->val <= lower || root->val >= upper)
+        {
+            return false;
+        }
+
+        return helper(root->left, lower, root->val) && helper(root->right, root->val, upper);
+    }
+    bool isValidBST(TreeNode* root) { return helper(root, LONG_MIN, LONG_MAX); }
+};
+
+TEST_CASE("Check Solution isValidBST method work successfully")
+{
+    Solution solution;
+    SECTION("True Branch")
+    {
+        TreeNode TreeNode1(1);
+        TreeNode TreeNode3(3);
+        TreeNode inputParm(2, &TreeNode1, &TreeNode3);
+
+        CAPTURE(inputParm);
+        REQUIRE(solution.isValidBST(&inputParm));
+    }
+    SECTION("False Branch")
+    {
+        TreeNode TreeNode1(1);
+        TreeNode TreeNode3(3);
+        TreeNode TreeNode6(6);
+
+        TreeNode TreeNode4(4, &TreeNode3, &TreeNode6);
+        TreeNode inputParm(5, &TreeNode1, &TreeNode4);
+
+        REQUIRE_FALSE(solution.isValidBST(&inputParm));
+    }
+}
