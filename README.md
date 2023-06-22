d951d939f388cacba48c93b5e73eb39a51de47b5
Layton <caolei6767@gmail.com>
2023-06-22 13:47:35 +0800

    add 0027

--- README.md
+++ README.md
@@ -162,6 +162,33 @@
        1. `while (left < right)`，这里使用 `<` ,因为 `left == right` 在区间 `[left, right)` 是没有意义的；
        2. `if (nums[middle] > target)`， right 更新为 middle，因为当前 `nums[middle]` 不等于 target，去左区间继续寻找，而寻找区间是左闭右开区间，所以 right 更新为 middle，即：下一个查询区间不会去比较 `nums[middle]`。
 
+5. [0027\_移除元素](https://leetcode.cn/problems/remove-element/)--[code](./Src/1_ArrayAndString/0027_RemoveElement.cpp):
+
+   给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+
+   不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并原地修改输入数组。
+
+   元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+   ```text
+   输入：nums = [3,2,2,3], val = 3
+   输出：2, nums = [2,2]
+   解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+
+   ----------------------------------------
+
+   输入：nums = [0,1,2,2,3,0,4,2], val = 2
+   输出：5, nums = [0,1,4,0,3]
+   解释：函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。注意这五个元素可为任意顺序。你不需要考虑数组中超出新长度后面的元素。
+
+   ```
+
+   - 解题思路：通过一个快指针和慢指针在一个 for 循环下完成两个 for 循环的工作。
+
+     - 双指针法（快慢指针法）在数组和链表的操作中是非常常见的，很多考察数组、链表、字符串等操作的面试题，都使用双指针法。
+
+     ![sample](https://code-thinking.cdn.bcebos.com/gifs/27.%E7%A7%BB%E9%99%A4%E5%85%83%E7%B4%A0-%E5%8F%8C%E6%8C%87%E9%92%88%E6%B3%95.gif)
+
 ### 二维数组
 
 1. [0048\_旋转矩阵](https://leetcode.cn/problems/rotate-image/)--[code](./Src/1_ArrayAndString/0048_RotateImage.cpp):
---
+++ Src/1_ArrayAndString/0027_RemoveElement.cpp
@@ -0,0 +1,52 @@
+#include "Tools/Tools.hpp"
+
+class Solution
+{
+public:
+    // 同方向双指针
+    int removeElement(vector<int>& nums, int val)
+    {
+        int slowIndex = 0;
+        for (size_t fastIndex = 0; fastIndex < nums.size(); fastIndex++) {
+            if (nums[fastIndex] != val) {
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        return slowIndex;
+    }
+    // 对向双指针
+    int removeElement_over(vector<int>& nums, int val)
+    {
+        int leftIndex = 0, rightIndex = nums.size() - 1;
+        while (leftIndex <= rightIndex) {
+            // 左指针找到不等于 val 的值
+            while (leftIndex <= rightIndex && nums[leftIndex] != val)
+                leftIndex++;
+            // 右指针找到等于 val 的值
+            while (leftIndex <= rightIndex && nums[rightIndex] == val)
+                rightIndex--;
+            // 交换两个值
+            if (leftIndex < rightIndex) {
+                swap(nums[leftIndex], nums[rightIndex]);
+                leftIndex++;
+                rightIndex--;
+            }
+        }
+        return leftIndex;
+    }
+};
+
+TEST_CASE("Check Solution removeElement method work successfully")
+{
+    Solution solution;
+
+    vector<int> param;
+
+    int target, result;
+    tie(param, target, result) = GENERATE(table<vector<int>, int, int>({
+        make_tuple(vector<int> { 3, 2, 2, 3 }, 3, 2),
+        make_tuple(vector<int> { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5),
+    }));
+
+    REQUIRE(solution.removeElement_over(param, target) == result);
+}
