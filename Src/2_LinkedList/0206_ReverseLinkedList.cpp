00be6d0f09f79278e80f6d27d6bcd56894f288b1
Layton <caolei6767@gmail.com>
2023-07-04 22:06:30 +0800

    add new ques

---
+++ Src/2_LinkedList/0206_ReverseLinkedList.cpp
@@ -0,0 +1,48 @@
+#include "Tools/ListNodeTools.h"
+
+#include <glog/logging.h>
+
+using namespace Catch;
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode* reverseList(ListNode* head)
+    {
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        ListNode* next = nullptr;
+        while (cur != nullptr) {
+            next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+};
+
+TEST_CASE("Check Solution reverseList method work successfully")
+{
+    Solution solution;
+
+    vector<int> param, result;
+    tie(param, result) = GENERATE(table<vector<int>, vector<int>>({
+        make_tuple(vector<int> { 1, 2, 6, 3, 4, 5, 6 }, vector<int> { 6, 5, 4, 3, 6, 2, 1 }),
+    }));
+    auto* Left = initListNode(param);
+    auto* resultNode = initListNode(result);
+    auto* ans = solution.reverseList(Left);
+    checkListNode(ans, resultNode);
+}
--- Src/2_LinkedList/README.md
+++ Src/2_LinkedList/README.md
@@ -23,3 +23,19 @@
 
    - 解题思路：链表操作的方式：1. 直接使用原来的链表进行删除操作。2. 设置一个虚拟头节点在进行删除。
      - 这两个操作方式的区别在于，是否需要对头节点进行特殊处理，第一种需要特殊处理头节点，而第二种则不需要。
+
+2. [0707\_设置链表](https://leetcode.cn/problems/design-linked-list/)--[code](./0707_DesignLinkedList.cpp):
+
+   单链表中的节点应该具备两个属性：val 和 next 。val 是当前节点的值，next 是指向下一个节点的指针/引用。
+
+   如果是双向链表，则还需要属性  prev  以指示链表中的上一个节点。假设链表中的所有节点下标从 0 开始。
+
+   - 解题思路：本体纯锻炼对于链表实现与操作的技巧。
+
+3. [0206\_反转链表](https://leetcode.cn/problems/reverse-linked-list/)--[code](./0206_ReverseLinkedList.cpp):
+
+   示例: 输入: 1->2->3->4->5->NULL 输出: 5->4->3->2->1->NULL
+
+   - 解题思路：通过双指针改变链表每个节点的指向，从指向后一个改为指向前一个节点即可完成翻转而不需要重新建立链表。
+
+   ![sample](https://code-thinking.cdn.bcebos.com/gifs/206.%E7%BF%BB%E8%BD%AC%E9%93%BE%E8%A1%A8.gif)
