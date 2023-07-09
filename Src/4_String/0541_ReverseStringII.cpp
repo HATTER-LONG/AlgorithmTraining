c642d126810b0d21cd923eb608ccbb6a043834c9
Layton <caolei6767@gmail.com>
2023-07-09 21:50:43 +0800

    add new

---
+++ Src/4_String/0541_ReverseStringII.cpp
@@ -0,0 +1,39 @@
+#include <catch2/catch_all.hpp>
+
+using namespace std;
+using namespace Catch;
+
+class Solution
+{
+public:
+    string reverseString(string s, int k)
+    {
+        // 每次步长为 2*k 长
+        for (int i = 0; i < static_cast<int>(s.length()); i += 2 * k) {
+
+            // 1. 每隔 2k 个字符的前 k 个字符进行反转
+            // 2. 剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符
+            if (i + k <= static_cast<int>(s.length())) {
+                reverse(s.begin() + i, s.begin() + i + k);
+            } else {
+                // 3. 剩余字符小于 k 个，反转剩余全部
+                reverse(s.begin() + i, s.end());
+            }
+        }
+        return s;
+    }
+};
+
+
+TEST_CASE("Check Solution 541reverseString method work successfully ", "[.]")
+{
+    Solution solution;
+
+    string input;
+    int target;
+    string result;
+    tie(input, target, result) = GENERATE(table<string, int, string>({
+        make_tuple("abcdefg", 2, "bacdfeg"),
+    }));
+    REQUIRE_THAT(solution.reverseString(input, target), Matchers::Equals(result));
+}
--- Src/4_String/README.md
+++ Src/4_String/README.md
@@ -19,3 +19,22 @@
    - 解题思路：双指针，没啥特殊的。
 
      ![sampel](https://code-thinking.cdn.bcebos.com/gifs/344.%E5%8F%8D%E8%BD%AC%E5%AD%97%E7%AC%A6%E4%B8%B2.gif)
+
+2. [0541\_反转字符串 II](https://leetcode.cn/problems/reverse-string-ii/)--[code](./0541_ReverseStringII.cpp):
+
+   给定一个字符串 s 和一个整数 k，从字符串开头算起，每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
+
+   - 如果剩余字符少于 k 个，则将剩余字符全部反转。
+   - 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+
+   ```test
+   输入：s = "abcdefg", k = 2
+   输出："bacdfeg"
+
+   ------------------------------------
+
+   输入：s = "abcd", k = 2
+   输出："bacd"
+   ```
+
+   - 解题思路：技巧是在遍历过程中步长可以设定为 2 \* k，这样只需要处理每次前进的字符串不需要单独记录字符数量。
